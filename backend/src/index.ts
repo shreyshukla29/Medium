42b115caa4583e3e8e72862ad0e4293fc4e8cd08
shrey <shreyshukla101217@gmail.com>
2025-01-24 10:04:28 +0530

    backend routes and controller setup for user and blogs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,36 +1,15 @@
-import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from 'hono'
+import { userRouter } from './routes/user';
+import { bookRouter } from './routes/blog';
 
-const prisma = new PrismaClient().$extends(withAccelerate());
-
-const app = new Hono<{
+export const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string;
-  };
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+  }
 }>();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/book', bookRouter)
 
-app.post("/signup", async (c) => {
-  try {
-    const body = await c.req.json();
-    if (!body.email || !body.password) {
-      return c.text("Email and password are required", 400);
-    }
-    await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-      },
-    });
-    return c.text("User created successfully!");
-  } catch (error) {
-    console.error("Error creating user:", error);
-    return c.text("Internal Server Error", 500);
-  }
-});
-
-export default app;
+export default app
